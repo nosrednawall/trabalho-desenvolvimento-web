a3e92c852b7b34ac2b8a029019a06570c7c48556
nosrednawall <nosrednawall@gmail.com>
2018-11-28 20:00:55 -0200

    terminado a questão 9

--- php/exercicio_09/exercicio_09.php
+++ php/exercicio_09/exercicio_09.php
@@ -1,17 +1,20 @@
 <?php
 
-// 8. Ao receber um número informe se ele é positivo, negativo ou nulo.
+// 9. Ao receber um número, informe se ele é divisível por 10, por 5, por 2 ou se não é divisível por
+// nenhum destes.
 
 
-    $numero_recebido = "-1";
+    $numero_recebido = "17";
     
     if(!$numero_recebido == null){
-        if($numero_recebido < 0){
-            echo("O número informado é negativo");
-        }else if($numero_recebido > 0){
-            echo("O número informado é positivo");
+        if($numero_recebido % 10 == 0){
+            echo("O número informado é dividível por 10");
+        }else if($numero_recebido % 5 == 0){
+            echo("O número informado é dividível por 5");
+        }else if($numero_recebido % 2 == 0){
+            echo("O número informado é dividível por 2");
         }else{
-            echo("O número informado foi 0");
+            echo("O número não é dividível por 10,5 e 2");
         }
     }else{
         echo("O número informado é nulo");
